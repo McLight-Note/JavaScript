0999f9a4b790fcf9a0da6997b72c579d20ee0f97
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-04-26 03:57:25 +0900

    +JS darslar

---
+++ 2_dars/2_dars.js
@@ -0,0 +1,26 @@
+let price = 10.99;
+let gpa = 4.5
+let firstName = 'Bro'
+let favFood = 'Shashlik'
+
+console.log(typeof gpa)
+console.log(`Your GPA is ${gpa}`)
+console.log(`The price is ${price}`)
+console.log(`Your name is ${firstName}`)
+console.log(`Your fav Food is ${favFood}`)
+
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`${firstName} is online: ${online}`)
+console.log(`Is this car for Sale: ${forSale}`)
+console.log(`Enrolled: ${isStudent}`)
+
+let fullName = 'Brother in Code';
+let age = 22;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
