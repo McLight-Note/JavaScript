b1c04b3a4d80b3cd543b2e5ff4509f9c2452e8d0
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-04-26 15:49:13 +0900

    +js new lessons learnt

---
+++ 3_dars/3_dars.js
@@ -0,0 +1,16 @@
+let students = 30;
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students **= 3;
+students %= 2;
+
+students++;
+students--;
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result)
+console.log(students)
---
+++ 5_dars/5_dars.js
@@ -0,0 +1,15 @@
+let age = window.prompt("How old are you?");
+age = Number(age);
+age += 1;
+
+let x, y, z = 'pizza';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+
+console.log(age, typeof age)
