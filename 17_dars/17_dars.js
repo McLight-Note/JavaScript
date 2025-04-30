216982fcd16f845b66ef4546f1e62902134b6aa3
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-05-01 08:49:28 +0900

    +dars

---
+++ 17_dars/17_dars.js
@@ -0,0 +1,26 @@
+const temp = -100;
+
+if(temp > 0 && temp <= 30){
+    console.log('Weather is good!');
+}
+else{
+    console.log("Weather is bad!")
+}
+
+const temp2 = 20;
+
+if(temp2 < 0 || temp2 > 30){
+    console.log('Weather is bad!');
+}
+else{
+    console.log("Weather is good!")
+}
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log('It is Cloudy!')
+}
+else{
+    console.log('It is Sunny!')
+}
